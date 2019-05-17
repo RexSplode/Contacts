637bb6f6363c7faecfec5cc2cb4b942da1c8011d
izhaldak <izhaldak@gloriumtech.com>
2019-05-17 10:07:27 +0300

    lesson 3

--- app/src/main/java/com/zhaldak/myapplication/ContactGroupsViewModel.kt
+++ app/src/main/java/com/zhaldak/myapplication/ContactGroupsViewModel.kt
@@ -1,7 +1,18 @@
 package com.zhaldak.myapplication
 
 import androidx.lifecycle.ViewModel;
+import com.zhaldak.myapplication.datadase.ContactGroup
 
 class ContactGroupsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    fun getContactGroups(): ArrayList<ContactGroup> {
+        return arrayListOf(
+            ContactGroup("Parents", "My parents and family", R.color.yellow),
+            ContactGroup("Work", "My colleagues and boss", R.color.blue),
+            ContactGroup("Friends", "My friends and schoolmates", R.color.pink),
+            ContactGroup("Someone I don't know", "Some people I met on the street", R.color.white),
+            ContactGroup("Other people of planet Earth", "Yeah, in case I will need to add them", R.color.colorPrimary),
+            ContactGroup("People from the Milky Way", "The galaxy is our common home", R.color.colorAccent)
+        )
+    }
+
 }
---
+++ app/src/main/java/com/zhaldak/myapplication/datadase/ContactGroup.kt
@@ -0,0 +1,10 @@
+package com.zhaldak.myapplication.datadase
+
+import com.zhaldak.myapplication.R
+
+class ContactGroup(
+    var name: String,
+    var description: String? = null,
+    var color: Int = R.color.colorPrimary
+) {
+}
